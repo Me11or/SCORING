da6fa07f0e48665e4ef8c4693a091bc669d9117e
Me11or <91011259+Me11or@users.noreply.github.com>
2022-01-06 16:14:16 +0300

    Доработки по теме Testing. Дизайн тестов

---
+++ tests/helper.py
@@ -0,0 +1,18 @@
+import functools
+
+
+def cases(cases):
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args):
+            for c in cases:
+                new_args = args + (c if isinstance(c, tuple) else (c,))
+                try:
+                    f(*new_args)
+                except Exception as e:
+                    e.args = e.args + ('Test case: %s has been failed' % str(c),)
+                    raise
+
+        return wrapper
+
+    return decorator
